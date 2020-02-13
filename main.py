528fbbd158d66a01074f8c0c7eec5f4a03a1338f
Gaven Rendell <gaven@rendello.ca>
2020-02-13 16:18:38 -0500

    init

---
+++ main.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3.7
+
+from pathlib import Path
+import json
+import tweepy
+
+
+def grab_keys(key_file):
+    with open(key_file, 'r') as tf:
+        return json.load(tf)
+
+
+def authenticate_new_user():
+    """ Manually authenticate a new user account.
+    """
+
+    auth = tweepy.OAuthHandler(keys["consumer_key"], keys["consumer_secret"])
+    redirect_url = auth.get_authorization_url()
+    print(redirect_url)
+
+    verifier = input('Verifier: ')
+
+    auth.request_token = {
+        'oauth_token': auth.request_token['oauth_token'],
+        'oauth_token_secret': verifier
+    }
+
+    auth.get_access_token(verifier)
+    print(f"acc token: {auth.access_token}")
+    print(f"acc secret: {auth.access_token_secret}")
+
+
+def authenticate(keys):
+    auth = tweepy.OAuthHandler(keys["consumer_key"], keys["consumer_secret"])
+    auth.set_access_token(keys["access_token"], keys["access_secret"])
+
+    return auth
+
+
+key_file = Path("~/.autotweetcart/keys.json").expanduser()
+keys = grab_keys(key_file)
+auth = authenticate(keys)
+api = tweepy.API(auth)
+
+user = api.me()
+print(user)
