0084bda761028338e81ea6b99f86362afefb2fdb
Gaven Rendell <gaven@rendello.ca>
2020-03-10 16:14:18 -0400

    starts to add tests w/ Hypothesis

---
+++ autotweetcart/test_back_end.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3.7
+
+from hypothesis import given, example
+from hypothesis.strategies import text
+
+# Local imports
+import back_end
+
+@given(s=text())
+def test_grab_title(s):
+    if not ("--" in s or "//" in s):
+        new_s = back_end.grab_title(s) 
+        assert new_s == s or new_s is None
+
+
+@given(s=text())
+@example(s="printh")
+@example(s="reload")
+@example(s="cstore")
+def test_intercept_restricted_tokens(s):
+    new_s = back_end.intercept_restricted_tokens(s)
+    if any(bad_token in s for bad_token in ["cstore", "reload", "printh"]):
+        assert s != new_s
+    else:
+        assert s == new_s
+
+
+if __name__ == "__main__":
+    test_grab_title()
+    test_intercept_restricted_tokens()
